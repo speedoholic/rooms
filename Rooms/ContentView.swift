43a42b272f97d564a7240b596c1fcf329af56c6c
Kushal Ashok <kushal.ashok@gmail.com>
2019-06-04 11:01:46 -0700

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Kushal Ashok on 6/4/19.
+//  Copyright © 2019 tradexport. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
